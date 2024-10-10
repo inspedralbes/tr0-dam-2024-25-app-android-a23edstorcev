ddbe65c546fce03c3ff232f356dc0bab3f63d835
a23edstorcev <156185747+a23edstorcev@users.noreply.github.com>
2024-10-10 09:54:03 +0200

    Delete app directory (No funciona)

--- app/app/src/main/java/com/example/p0/ApiService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.p0
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("/api/questions")  // Cambia esta ruta según la ruta de tu servidor Node.js
-    fun getQuestions(): Call<List<Question>>
-}
--- app/app/src/main/java/com/example/p0/Question.kt
+++
@@ -1,7 +0,0 @@
-package com.example.p0
-
-data class Question(
-    val id: Int,
-    val pregunta: String,
-    val resposta: List<String>
-)
